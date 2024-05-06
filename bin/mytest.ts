eff27883912ffee5de21bf83120ce3a94b8c0b49
Xia Zhao <xazhao@amazon.com>
2024-05-06 16:57:58 -0700

    Change source

--- bin/mytest.ts
+++ bin/mytest.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { MytestStack } from '../lib/mytest-stack';
 
 const app = new cdk.App();
+
 new MytestStack(app, 'MytestStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -11,7 +12,7 @@ new MytestStack(app, 'MytestStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- lib/mytest-stack.ts
+++ lib/mytest-stack.ts
@@ -1,16 +1,68 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as aws_s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+const app = new cdk.App();
+
+export class BucketStack extends cdk.Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const encryptionKey = new kms.Key(this, 'ArtifactsBucketEncryptionKey');
+    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+      encryptionKey,
+      encryption: s3.BucketEncryption.KMS,
+    });
+    new aws_s3_deployment.BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [aws_s3_deployment.Source.asset('lib')]
+    });
+
+  }
+
+}
+class MyApplication extends cdk.Stage {
+  constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    new BucketStack(this, 'bucket-stack');
+
+  }
+}
 
 export class MytestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      crossAccountKeys: true,
+      synth: new pipelines.ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: pipelines.CodePipelineSource.gitHub('xazhao/TestCDK', 'main'),
+        commands: [
+          'aws sts get-caller-identity'
+          // 'npm ci',
+          // 'npm run build',
+          // 'npx cdk synth',
+        ],
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MytestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 'MyApplication' is defined below. Call `addStage` as many times as
+    // necessary with any account and region (may be different from the
+    // pipeline's).
+    pipeline.addStage(new MyApplication(this, 'Prod', {
+      env: {
+        account: '714425508342',
+        region: 'us-west-2',
+      }
+    }));
   }
+  
 }
